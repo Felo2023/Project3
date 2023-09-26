99e9468338085b78459801da3b73a83becdbc90b
Felo2023 <134896499+Felo2023@users.noreply.github.com>
2023-09-25 21:54:09 -0600

    Display all tables with one simple graph

--- Programs/html/Javascript/Readflaskdata4.0.js
+++ Programs/html/Javascript/Readflaskdata4.0.js
@@ -2,27 +2,29 @@ document.addEventListener("DOMContentLoaded", function () {
     const globalTab = document.getElementById("global-tab");
     const canadaTab = document.getElementById("canada-tab");
     const egyptTab = document.getElementById("egypt-tab");
+    const ukTab = document.getElementById("uk-tab"); // New tab
+    const australiaTab = document.getElementById("australia-tab"); // New tab
     const movieTable = document.getElementById("movie-data");
     const nextButton = document.getElementById("next-button");
     const previousButton = document.getElementById("previous-button");
+    const barChartDiv = document.getElementById("bar-chart");
 
     let currentPage = 0; // Current page for pagination
     const pageSize = 10; // Number of rows per page
     let currentTabId = "global1"; // Initialize with the default tab
 
     globalTab.addEventListener("click", () => loadMovieData("global1"));
-    canadaTab.addEventListener("click", () => loadMovieData("canada1"));
-    egyptTab.addEventListener("click", () => loadMovieData("egypt1"));
+    canadaTab.addEventListener("click", () => loadMovieData("canada"));
+    egyptTab.addEventListener("click", () => loadMovieData("egypt"));
+    ukTab.addEventListener("click", () => loadMovieData("united_kingdom")); // New tab
+    australiaTab.addEventListener("click", () => loadMovieData("australia")); // New tab
     nextButton.addEventListener("click", () => nextPage());
     previousButton.addEventListener("click", () => previousPage());
 
-
-
-    
     function loadMovieData(apiEndpoint) {
         currentTabId = apiEndpoint; // Update the current tab
         // Make a request to your API endpoint using fetch or jQuery.ajax
-        fetch(`http://127.0.0.1:5000/api/v1.0/global1`)
+        fetch(`http://127.0.0.1:5000/api/v1.0/${apiEndpoint}`)
             .then((response) => response.json())
             .then((data) => {
                 // Clear previous data
@@ -32,9 +34,12 @@ document.addEventListener("DOMContentLoaded", function () {
                 const startIndex = currentPage * pageSize;
                 const endIndex = startIndex + pageSize;
 
+                // Get data for the current page
+                const currentPageData = data.slice(startIndex, endIndex);
+
                 // Populate the table with the retrieved data for the current page
-                for (let i = startIndex; i < endIndex && i < data.length; i++) {
-                    const movie = data[i];
+                for (let i = 0; i < currentPageData.length; i++) {
+                    const movie = currentPageData[i];
                     const row = document.createElement("tr");
                     row.innerHTML = `
                         <td>${movie.No}</td>
@@ -46,6 +51,9 @@ document.addEventListener("DOMContentLoaded", function () {
                     `;
                     movieTable.appendChild(row);
                 }
+
+                // Update the bar chart with the current page data
+                updateBarChart(currentPageData);
             })
             .catch((error) => {
                 console.error("Error fetching data:", error);
@@ -69,23 +77,22 @@ document.addEventListener("DOMContentLoaded", function () {
     function updateBarChart(data) {
         const weeksAtTop10 = data.map((movie) => movie.week_at_Top10);
         const movieNames = data.map((movie) => movie.movie);
-    
+
         const chartData = [{
             x: movieNames,
             y: weeksAtTop10,
             type: 'bar',
         }];
-    
+
         const layout = {
             title: 'Weeks at Top 10',
             xaxis: { title: 'Movie' },
             yaxis: { title: 'Weeks' },
         };
-    
-        Plotly.newPlot('bar-chart', chartData, layout);
+
+        // Use Plotly.react to update the chart
+        Plotly.react(barChartDiv, chartData, layout);
     }
-
-
 
     // Load data for the default tab (e.g., "global")
     loadMovieData("global1");
